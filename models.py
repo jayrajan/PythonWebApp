65c0bf79895b3ebab804b20052210cbbeb57bee4
Jerin Rajan <jrajan@jprtech.co.uk>
2020-04-15 22:01:35 +0100

    First commit

---
+++ models.py
@@ -0,0 +1,2 @@
+# Code written by Jerin Rajan on 15th April 2020
+# models.py - handles everything that involves a Database.
---
+++ service.py
@@ -0,0 +1,2 @@
+#  Code written by Jerin Rajan on 15th April 2020
+# service.py - converts the request into a response
