b3228bd51207f8bae0e8cca032ab38391892f730
Jerin Rajan <jrajan@jprtech.co.uk>
2020-04-15 22:01:06 +0100

    added new feat: hello_name()

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-#  Code written by Jerin Rajan on 15th April 2019
+#  Code written by Jerin Rajan on 15th April 2020
+#  app.py - this file contains code that handles the
+# entry & exit point to our application
+
 # import Flask
 from flask import Flask
 
 # create an app instance
 app = Flask(__name__)
 
-# at the end point /
-@app.route("/")
+# # at the end point /
+# @app.route("/")
+#
+# # call method hello
+# def hello_():
+#     # returns "Hello world"
+#     return "Hello World!"
 
-# call method hello
-def hello():
-    # returns "Hello world"
-    return "Hello World!"
+# at the end point /<name>
+@app.route("/<name>")
+# call method hello_name
+def hello_name(name):
+    # which returns "hello + name"
+    return "Hello "+ name
 
 # on running python app.py
 if __name__ == '__main__':
     # run the flask app
-    app.run()
+    # app.run()
+    app.run(debug=True)
